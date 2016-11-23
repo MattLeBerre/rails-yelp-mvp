cfd8108c09c1cd98f2585de54fbd018127086398
Matthieu Le Berre <matthieu.leberre@gmail.com>
2016-11-23 17:39:08 +0100

    ok

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
 
-  validates :rating, inclusion: { in: (0..5), allow_nil: false }
+  validates :rating, inclusion: { in: (0..5), allow_nil: false },numericality: { only_integer: true }
   validates :content, presence: true
 end
